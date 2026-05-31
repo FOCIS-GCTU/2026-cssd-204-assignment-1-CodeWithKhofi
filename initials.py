1e734c99abb1e8635a05f1dc133f95c54b88a6be
CodeWithKhofi <codewithkhofi@gmail.com>
2026-05-31 01:06:20 +0000

    Finish assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out stylized initials JMN with instructed grid dimensions.
+# Assignment Number: 1
+#
+# Name: Joseph Nyame
+# STUDENT ID: 2425402996
+# Email: josephnyame1305@gmail.com
+# Grader: Augustus
+#
+# On my honor, Joseph Nyame, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
+def main(): #My main function
+
+
+    print("")
+    print("...JMN")
+    print("")
+    print("...JJJJJJJJJJJJ........MMMM....MMMM........NNN......N.....")
+    print(".........JJJJJJ........MMMM....MMMM........NNNN.....N.....")
+    print(".........JJJJJJ........MM.MM..MM.MM........NNNN.....N.....")
+    print(".........JJJJJJ........MM.MM..MM.MM........NN.NN....N.....")
+    print(".........JJJJJJ........MM..MMMM..MM........NN..NN...N.....")
+    print(".........JJJJJJ........MM..MMMM..MM........NN...NN..N.....")
+    print("...J.....JJJJJJ........MM...MM...MM........NN....NN.N.....")
+    print("...JJ....JJJJJJ........MM...MM...MM........NN.....NNN.....")
+    print("...JJJJJJJJJJJJ...**...MM........MM...**...NN.....NNN...**")
+    print("......JJJJJJ......**...MM........MM...**...NN.....NNN...**")
+    print("")
 
 
 main()
+
+
